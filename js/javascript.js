a103e20bbcca529a1f393f4111443fb1126496ad
solecibils <116587431+solecibils@users.noreply.github.com>
2022-12-03 14:09:06 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function inclusive()
+                  {document.body.style.backgroundImage = "none";
+                    document.body.style.backgroundColor = "black";
+                    document.body.style.color= "white";
+}
+
+function copyright() {alert("Página diseñada por Sole Cibils - solecibils@gmail.com")}
